9aa20dba980770b3eb87c89e1f55689848d3e4ae
Narendra Choudhary <narendrachoudhary.ai@gmail.com>
2025-11-03 14:08:37 +0000

    Practice file handling topic

---
+++ FileHandling/AppendMode.py
@@ -0,0 +1,4 @@
+# Append Mode 
+file = open("myfile.txt", "a")
+file.write("\nThis line is added later.")
+file.close()
---
+++ FileHandling/ReadMode.py
@@ -0,0 +1,22 @@
+#  Read Mode 
+file = open('myfile.txt','r')
+text = file.read()
+print(text)
+file.close()
+
+#  Read Line
+f = open("myfile.txt", "r")
+line1 = f.readline()
+line2 = f.readline()
+line3 = f.readline()
+f.close()
+
+print("Line 1: ", line1)
+# print("Line 2: ", line2)
+print("Line 3: ", line3)
+
+#  Read Lines
+f = open("myfile.txt", "r")
+lines = f.readlines()
+f.close()
+print(lines)
